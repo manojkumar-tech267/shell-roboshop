8593fc0e1e5bdaed9ac0c0fb45ecca549b814d75
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2025-09-08 20:54:39 +0530

    added catalogue file

--- catalogue.sh
+++ catalogue.sh
@@ -1,48 +1,50 @@
 #!/bin/bash 
 
 userid=$(id -u)
-Script_Dir=$PWD
+script_dir=$PWD 
+
+logs_folder="/var/log/roboshop-logs"
+script_name=$(echo $0 | cut -d "." -f1)
+log_file="$logs_folder/$script_name.sh"
+
+mkdir -p logs_folder
 
 if [ $userid -ne 0 ]
 then 
-    echo "You are not a root user run with root access"
+    echo "You are not a root user Please run with root access!!!"
     exit 1 
 else 
-    echo "You are already a root user"
+    echo "You are running with root user access"
 fi 
-
-Logs_Folder="/var/log/roboshop-logs"
-File_Name=$(echo $0 | cut -d "." -f1)
-Log_File=$Logs_Folder/$File_Name.log
 
 VALIDATE()
 {
-    if [ $? -eq 0 ]
+    if [ $1 -eq 0 ]
     then 
-        echo "$2 is successful"
+        echo "$2 is success"
     else 
-        echo "$2 is not successful"
-        exit 1
+        echo "$2 is failed"
+        exit 1 
     fi
 }
 
 dnf module disable nodejs -y
-VALIDATE $? "Disabling NodeJs"
+VALIDATE $? "Disabling Nodejs"
 
 dnf module enable nodejs:20 -y
-VALIDATE $? "Enabling NodeJs 20 version"
+VALIDATE $? "Enabling Nodejs"
 
 dnf install nodejs -y
 VALIDATE $? "Installing NodeJs"
 
-mkdir -p /app 
+mkdir -p /app
 VALIDATE $? "Creating App directory"
 
 id roboshop 
 if [ $? -ne 0 ]
 then 
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
-    VALIDATE $? "Creating roboshop system user"
+    VALIDATE $? "Created roboshop user"
 else 
     echo "Roboshop user is already there we are skipping"
 fi 
@@ -50,37 +52,35 @@ fi
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
 VALIDATE $? "Downloading catalogue code"
 
-cd /app 
-unzip /tmp/catalogue.zip
-VALIDATE $? "Extracting catalogue code"
-
+rm -rf /app/*
 cd /app
-npm install
+unzip /tmp/catalogue.zip
+VALIDATE $? "unzipping catalogue"
+
+npm install 
 VALIDATE $? "Installing NodeJs dependencies"
 
-cp $Script_Dir/catalogue.service /etc/systemd/system/catalogue.service
-VALIDATE $? "Copying catalogue service"
+cp $script_dir/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Copying catalogue.service file"
 
 systemctl daemon-reload
-VALIDATE $? "Reload catalogue"
+VALIDATE $? "Reload catalogue service"
 
 systemctl enable catalogue 
 systemctl start catalogue
-VALIDATE $? "Enable and Start Catalogue service"
+VALIDATE $? "Enable and start catalogue"
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-VALIDATE $? "Copying mongo repo file"
+cp $script_dir/catalogue.service /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copy mongo.repo file"
 
 dnf install mongodb-mongosh -y
-VALIDATE $? "Installing Mongodb client"
+VALIDATE $? "Installing mongodb client"
 
-Status=$(mongosh --host mongodb.cloudwithmanoj.online --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
-
-if [ $Status -lt 0 ]
-then 
+status=$(mongosh --host mongodb.cloudwithmanoj.online --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+if [ $status -lt 0 ]
+then
     mongosh --host mongodb.cloudwithmanoj.online </app/db/master-data.js
-    VALIDATE $? "Loading Master Data in to MongoDb"
+    VALIDATE $? "Loading master data"
 else 
     echo "Data is already loaded we are skipping"
 fi
-
