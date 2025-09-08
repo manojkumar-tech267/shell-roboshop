01a90f1d08fc1901d978680c2c4f0a5d441b32e7
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2025-09-08 21:21:54 +0530

    added log files

--- catalogue.sh
+++ catalogue.sh
@@ -11,30 +11,30 @@ mkdir -p logs_folder
 
 if [ $userid -ne 0 ]
 then 
-    echo "You are not a root user Please run with root access!!!"
+    echo "You are not a root user Please run with root access!!!" | tee -a $logs_folder
     exit 1 
 else 
-    echo "You are running with root user access"
+    echo "You are running with root user access" | tee -a $logs_folder
 fi 
 
 VALIDATE()
 {
     if [ $1 -eq 0 ]
     then 
-        echo "$2 is success"
+        echo "$2 is success" | tee -a $logs_folder
     else 
-        echo "$2 is failed"
+        echo "$2 is failed" | tee -a $logs_folder
         exit 1 
     fi
 }
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>> $logs_folder
 VALIDATE $? "Disabling Nodejs"
 
-dnf module enable nodejs:20 -y
+dnf module enable nodejs:20 -y &>> $logs_folder
 VALIDATE $? "Enabling Nodejs"
 
-dnf install nodejs -y
+dnf install nodejs -y &>> $logs_folder
 VALIDATE $? "Installing NodeJs"
 
 mkdir -p /app
@@ -46,24 +46,24 @@ then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
     VALIDATE $? "Created roboshop user"
 else 
-    echo "Roboshop user is already there we are skipping"
+    echo "Roboshop user is already there we are skipping" | tee -a $logs_folder
 fi 
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  &>> $logs_folder
 VALIDATE $? "Downloading catalogue code"
 
 rm -rf /app/*
 cd /app
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>> $logs_folder
 VALIDATE $? "unzipping catalogue"
 
-npm install 
+npm install &>> $logs_folder
 VALIDATE $? "Installing NodeJs dependencies"
 
 cp $script_dir/catalogue.service /etc/systemd/system/catalogue.service
 VALIDATE $? "Copying catalogue.service file"
 
-systemctl daemon-reload
+systemctl daemon-reload &>> $logs_folder
 VALIDATE $? "Reload catalogue service"
 
 systemctl enable catalogue 
@@ -73,7 +73,7 @@ VALIDATE $? "Enable and start catalogue"
 cp $script_dir/catalogue.service /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Copy mongo.repo file"
 
-dnf install mongodb-mongosh -y
+dnf install mongodb-mongosh -y &>> $logs_folder
 VALIDATE $? "Installing mongodb client"
 
 status=$(mongosh --host mongodb.cloudwithmanoj.online --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
