07ef4d711fa8d1d6060ad9a39a36d97c7798fe6c
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2025-10-14 20:22:58 +0530

    added files

--- roboshop.sh
+++ roboshop.sh
@@ -2,14 +2,13 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-06933f730773e2191"
-INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 ZONE_ID="Z01067621LHUD88I43HR3"
 DOMAIN_NAME="cloudwithmanoj.online" # replace with your domain
 
 #for instance in ${INSTANCES[@]}
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
